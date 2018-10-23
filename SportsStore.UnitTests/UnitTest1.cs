6a5adc82330936a5a0c29741d42f27cd02ba4e3f
Joseph Callahan <Joseph.callahan1@outlook.com>
2018-10-23 11:56:54 -0400

    Create Cart & Button Navigation

--- SportsStore.UnitTests/UnitTest1.cs
+++ SportsStore.UnitTests/UnitTest1.cs
@@ -17,7 +17,7 @@ namespace SportsStore.UnitTests
         [TestMethod]
         public void Can_Paginate()
         {
-           
+
             Mock<IProductRepository> mock = new Mock<IProductRepository>();
             mock.Setup(m => m.Products).Returns(new Product[] {
                 new Product {ProductID = 1, Name = "P1"},
@@ -28,10 +28,10 @@ namespace SportsStore.UnitTests
             });
             ProductController controller = new ProductController(mock.Object);
             controller.PageSize = 3;
-            
+
             ProductsListViewModel result =
-                (ProductsListViewModel)controller.List(2).Model;
-          
+                (ProductsListViewModel)controller.List(null, 2).Model;
+
             Product[] prodArray = result.Products.ToArray();
             Assert.IsTrue(prodArray.Length == 2);
             Assert.AreEqual(prodArray[0].Name, "P4");
@@ -40,34 +40,141 @@ namespace SportsStore.UnitTests
 
 
         [TestMethod]
-        public void Can_Generate_Page_Links()
+        public void Can_Send_Pagination_View_Model()
         {
-            // Arrange - define an HTML helper - we need to do this
-            // in order to apply the extension method
-            HtmlHelper myHelper = null;
-            // Arrange - create PagingInfo data
-            PagingInfo pagingInfo = new PagingInfo
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
             {
-                CurrentPage = 2,
-                TotalItems = 28,
-                ItemsPerPage = 10
-            };
+                new Product {ProductID = 1, Name = "P1" },
+                new Product {ProductID = 2, Name = "P2"},
+                new Product {ProductID = 3, Name = "P3"},
+                new Product {ProductID = 4, Name = "P4"},
+                new Product {ProductID = 5, Name = "P5"}
+            });
+            ProductController controller = new
+                ProductController(mock.Object);
+            controller.PageSize = 3;
 
-            // Arrange - set up the delegate using a lambda expression
-            Func<int, string> pageUrlDelegate = i => "Page" + i;
+            ProductsListViewModel result
+                = (ProductsListViewModel)controller.List(null, 2).Model;
 
-            // Act
-            MvcHtmlString result = myHelper.PageLinks(pagingInfo, pageUrlDelegate);
+            PagingInfo pageInfo = result.PagingInfo;
+            Assert.AreEqual(pageInfo.CurrentPage, 2);
+            Assert.AreEqual(pageInfo.ItemsPerPage, 3);
+            Assert.AreEqual(pageInfo.TotalItems, 5);
+            Assert.AreEqual(pageInfo.TotalPages, 2);
+        }
+        [TestMethod]
+        public void Can_Filter_Products()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product {ProductID = 1, Name = "P1",
+                Category = "Cat1" },
+                new Product {ProductID = 2, Name = "P2",
+                Category = "Cat2" },
+                new Product {ProductID = 3, Name = "P3",
+                Category = "Cat1" },
+                new Product {ProductID = 4, Name = "P4",
+                Category = "Cat2" },
+                new Product {ProductID = 5, Name = "P5",
+                Category = "Cat3" }
+            });
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
 
-            // Assert
-            Assert.AreEqual(@"<a class=""btn btn-default"" href=""Page1"">1</a>"
-       + @"<a class=""btn btn-default btn-primary selected"" href=""Page2"">2</a>"
-                + @"<a class=""btn btn-default"" href=""Page3"">3</a>",
-                result.ToString());
+            Product[] result = ((ProductsListViewModel)controller.List("Cat2", 1).Model)
+                .Products.ToArray();
+
+            Assert.AreEqual(result.Length, 2);
+            Assert.IsTrue(result[0].Name == "P2" && result[0].Category == "Cat2");
         }
 
+        public void Can_Create_Categories()
+        {
 
+                // Arrange
+                // - create the mock repository
+                Mock<IProductRepository> mock = new Mock<IProductRepository>();
+                mock.Setup(m => m.Products).Returns(new Product[] {
+            new Product {ProductID = 1, Name = "P1", Category = "Apples"},
+            new Product {ProductID = 2, Name = "P2", Category = "Apples"},
+            new Product {ProductID = 3, Name = "P3", Category = "Plums"},
+            new Product {ProductID = 4, Name = "P4", Category = "Oranges"},
+            });
 
+            NavController target = new NavController(mock.Object);
 
+            // Act = get the set of categories
+            string[] results = ((IEnumerable<string>)target.Menu().Model).ToArray();
+
+            // Assert
+            Assert.AreEqual(results.Length, 3);
+            Assert.AreEqual(results[0], "Apples");
+            Assert.AreEqual(results[1], "Oranges");
+            Assert.AreEqual(results[2], "Plums");
+        }
+
+        [TestMethod]
+        public void Indicates_Selected_Category()
+        {
+
+            // Arrange
+            // - create the mock repository
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] {
+        new Product {ProductID = 1, Name = "P1", Category = "Apples"},
+        new Product {ProductID = 4, Name = "P2", Category = "Oranges"},
+    });
+
+            // Arrange - create the controller
+            NavController target = new NavController(mock.Object);
+
+            // Arrange - define the category to selected
+            string categoryToSelect = "Apples";
+
+            // Action
+            string result = target.Menu(categoryToSelect).ViewBag.SelectedCategory;
+
+            // Assert
+            Assert.AreEqual(categoryToSelect, result);
+        }
+
+        [TestMethod]
+        public void Generate_Category_Specific_Product_Count()
+        {
+
+            // Arrange
+            // - create the mock repository
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] {
+        new Product {ProductID = 1, Name = "P1", Category = "Cat1"},
+        new Product {ProductID = 2, Name = "P2", Category = "Cat2"},
+        new Product {ProductID = 3, Name = "P3", Category = "Cat1"},
+        new Product {ProductID = 4, Name = "P4", Category = "Cat2"},
+        new Product {ProductID = 5, Name = "P5", Category = "Cat3"}
+    });
+
+            // Arrange - create a controller and make the page size 3 items
+            ProductController target = new ProductController(mock.Object);
+            target.PageSize = 3;
+
+            // Action - test the product counts for different categories
+            int res1 = ((ProductsListViewModel)target
+                .List("Cat1").Model).PagingInfo.TotalItems;
+            int res2 = ((ProductsListViewModel)target
+                .List("Cat2").Model).PagingInfo.TotalItems;
+            int res3 = ((ProductsListViewModel)target
+                .List("Cat3").Model).PagingInfo.TotalItems;
+            int resAll = ((ProductsListViewModel)target
+                .List(null).Model).PagingInfo.TotalItems;
+
+            // Assert
+            Assert.AreEqual(res1, 2);
+            Assert.AreEqual(res2, 2);
+            Assert.AreEqual(res3, 1);
+            Assert.AreEqual(resAll, 5);
+        }
     }
 }
